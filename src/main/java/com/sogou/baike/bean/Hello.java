8c890379166e02c9a83b7e9e22c5c16cf5ea228a
songpanfei <571354831@qq.com>
2018-02-01 16:10:06 +0800

    sdfsdssdf

--- src/main/java/com/sogou/baike/bean/Hello.java
+++ src/main/java/com/sogou/baike/bean/Hello.java
@@ -5,7 +5,7 @@ package com.sogou.baike.bean;
  */
 public class Hello {
     public static void main(String args[]){
-        System.out.print("2ss22");
+        System.out.print("2ss221");
 
     }
 }
