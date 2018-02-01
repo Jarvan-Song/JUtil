59d095a927996a445e75a387fa852d13caf82513
songpanfei <571354831@qq.com>
2018-02-01 15:28:43 +0800

    [first commit]

---
+++ src/main/java/com/sogou/baike/bean/Hello.java
@@ -0,0 +1,10 @@
+package com.sogou.baike.bean;
+
+/**
+ * Created by songpanfei on 15:28 2018/2/1.
+ */
+public class Hello {
+    public static void main(String args[]){
+        System.out.print("222");
+    }
+}
