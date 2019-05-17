b5c23d5e2e2c0d58d6bebe7fb8374bb148319cff
songpanfei <571354831@qq.com>
2019-05-17 14:29:18 +0800

    txt下载代码提交

---
+++ src/main/java/util/txt/Pv.java
@@ -0,0 +1,75 @@
+package util.txt;
+
+import java.io.Serializable;
+
+/**
+ * Created by songpanfei on 2019-05-17.
+ */
+public class Pv implements Serializable {
+
+    private long lemmaId;
+    private int date;
+    private int pvPc;
+    private int pvMb;
+
+    private String lemmaTitle; //额外字段
+
+    public String getLemmaTitle() {
+        return lemmaTitle;
+    }
+
+    public void setLemmaTitle(String lemmaTitle) {
+        this.lemmaTitle = lemmaTitle;
+    }
+
+    public long getLemmaId() {
+        return lemmaId;
+    }
+
+    public void setLemmaId(long lemmaId) {
+        this.lemmaId = lemmaId;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getPvPc() {
+        return pvPc;
+    }
+
+    public void setPvPc(int pvPc) {
+        this.pvPc = pvPc;
+    }
+
+    public int getPvMb() {
+        return pvMb;
+    }
+
+    public void setPvMb(int pvMb) {
+        this.pvMb = pvMb;
+    }
+
+    public String getFormat(){
+        return  lemmaId + "\t"+
+                lemmaTitle  +"\t"+
+                pvPc +"\t"+
+                pvMb +"\t"+
+                date;
+    }
+
+    @Override
+    public String toString() {
+        return "Pv{" +
+                "lemmaId=" + lemmaId +
+                ", date=" + date +
+                ", pvPc=" + pvPc +
+                ", pvMb=" + pvMb +
+                ", lemmaTitle='" + lemmaTitle + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/util/txt/TxtUtils.java
@@ -0,0 +1,43 @@
+package util.txt;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+/**
+ * Created by songpanfei on 2019-05-17.
+ * 下载txt，以\t为分隔符，方便粘贴到excel
+ * 有点：文件小速度快
+ */
+public class TxtUtils {
+    private static String header = "lemmaId" + "\t" + "lemmaTitle" + "\t" + "pvPv" + "\t" + "pvMb" + "\t" + "date";
+
+    public String downTxt(HttpServletRequest request, HttpServletResponse response, List<Pv> list, String fileName) {
+        long fileLength = -1;
+        response.setHeader("Content-Length", String.valueOf(fileLength));
+        response.setHeader("Content-disposition", "attachment; filename=" + fileName);
+        response.setContentType("application/octet-stream; charset=gbk");
+        PrintWriter printWriter = null;
+        try {
+            printWriter = response.getWriter();
+            printWriter.print(header + "\n");
+            if (list != null && list.size() > 0) {
+                for (Pv pvBean : list) {
+                    if (pvBean != null) {
+                        printWriter.print(pvBean.getFormat() + "\n");
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (printWriter != null) {
+                printWriter.flush();
+                printWriter.close();
+            }
+        }
+        return null;
+    }
+}
