6f61a76491cf4aaad51db01b90b536cebb258f4f
songpanfei <571354831@qq.com>
2019-09-24 19:29:34 +0800

    leetcode刷题

--- src/main/java/util/leetcode/Solution.java
+++ src/main/java/util/leetcode/Solution.java
@@ -1,5 +1,6 @@
 package util.leetcode;
 
+import java.util.ArrayDeque;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -574,5 +575,37 @@ public class Solution {
         }else {
             return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         }
+    }
+
+    public boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (q == null || p == null) return false;
+        if (p.val != q.val) return false;
+        return true;
+    }
+
+    public boolean isSameTree2(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (!check(p, q)) return false;
+        ArrayDeque<TreeNode> deqP = new ArrayDeque<>();
+        ArrayDeque<TreeNode> deqQ = new ArrayDeque<>();
+        deqP.addLast(p);
+        deqQ.addLast(q);
+        while (!deqP.isEmpty()) {
+            p = deqP.removeFirst();
+            q = deqQ.removeFirst();
+            if (!check(p, q)) return false;
+            if (!check(p.left, q.left)) return false;
+            if (p.left != null) {
+                deqP.addLast(p.left);
+                deqQ.addLast(q.left);
+            }
+            if (!check(p.right, q.right)) return false;
+            if (p.right != null) {
+                deqP.addLast(p.right);
+                deqQ.addLast(q.right);
+            }
+        }
+        return true;
     }
 }
