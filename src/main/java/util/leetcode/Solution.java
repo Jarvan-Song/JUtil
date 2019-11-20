af7e5fac5c4e725fcf020c67b4ea7ffd956001ff
songpanfei <571354831@qq.com>
2019-11-20 16:14:54 +0800

    leetcode刷题

--- src/main/java/util/leetcode/Solution.java
+++ src/main/java/util/leetcode/Solution.java
@@ -2809,4 +2809,21 @@ public class Solution {
             list.add(people[i][1], people[i]);
         return list.toArray(people);
     }
+
+    /*
+    Given a list of daily temperatures T, return a list such that, for each day in the input, tells you how many days you would have to wait until a warmer temperature. If there is no future day for which this is possible, put 0 instead.
+
+    For example, given the list of temperatures T = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+
+    Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
+     */
+
+    public int[] dailyTemperatures(int[] T) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<T.length;i++){
+            map.put(T[i], i);
+        }
+
+        return null;
+    }
 }
--- src/main/java/util/leetcode/SortAlgorithms.java
+++ src/main/java/util/leetcode/SortAlgorithms.java
@@ -1,36 +1,21 @@
 package util.leetcode;
 
+import com.github.houbb.opencc4j.util.ZhConverterUtil;
+
 /**
  * Created by songpanfei on 2019-09-23.
  */
 public class SortAlgorithms {
     public static void main(String[] args){
         int[] a = new int[]{1,11,3,9,5,6};
-        insertionSort2(a);
+        charu1118(a);
         for(int i=0;i<a.length;i++){
             System.out.println(a[i]);
         }
+        System.out.println(ZhConverterUtil.convertToSimple("幺儿"));
     }
 
     public static int[] maopao(int[] arry){
-        for(int i=0;i<arry.length-1;i++){
-            boolean flag = false;
-            for(int j=0;j<arry.length-1-i;j++){
-                int pre = arry[j];
-                int nex = arry[j+1];
-                if(pre > nex){
-                    flag = true;
-                    int tep = pre;
-                    arry[j] = nex;
-                    arry[j+1] = tep;
-                }
-            }
-            if(!flag) break;
-        }
-        return arry;
-    }
-
-    public static int[] maopao2(int[] arry){
         for(int i=arry.length-1;i>0;i--){
             boolean flag = false;
             for(int j=0;j<i;j++){
@@ -64,20 +49,6 @@ public class SortAlgorithms {
     }
 
     public static void insertionSort(int[] arr){
-        for (int i=1; i<arr.length; ++i){
-            int value = arr[i];
-            int position=i;
-            while (position>0 && value < arr[position-1]){
-                arr[position] = arr[position-1];
-                position--;
-            }
-            arr[position] = value;
-        }
-    }
-
-
-
-    public static void insertionSort2(int[] arr){
         for(int i=1;i<arr.length;i++){
             int key = i;
             int value = arr[i];
@@ -88,5 +59,123 @@ public class SortAlgorithms {
             arr[key] = arr[i];
         }
     }
+
+    public static void kuaisu(int[] array, int low, int high){
+        if(low >= high){return;}
+        int avr = helper(array, low, high);
+        kuaisu(array, low, avr-1);
+        kuaisu(array, avr + 1, high);
+    }
+
+    public static int helper(int[] array, int low, int high){
+        int p = array[low];
+        while (low < high){
+            while (low < high && array[high] > p) high--;
+            array[low] = array[high];
+            while (low < high && array[low] < p) low++;
+            array[high]  = array[low];
+        }
+        array[low] = p;
+        return low;
+    }
+
+
+    public static void maopao1118(int[] array){
+        for(int i = array.length-1;i>0;i--){
+            boolean flag = false;
+            for(int j = 0;j<i;j++){
+                if(array[j] > array[j+1]){
+                    flag = true;
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+            if(!flag){
+                break;
+            }
+        }
+    }
+
+    public static void xuanze1118(int[] array){
+        for(int i=0;i<array.length-1;i++){
+            int min = i;
+            for(int j = i + 1;j<array.length;j++){
+                if(array[min] > array[j]){
+                    min = j;
+                }
+            }
+            if(min != i){
+                int temp = array[min];
+                array[min] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+
+    public static void charu1118(int[] array){
+        for(int i=1;i<array.length;i++){
+            int key = i;
+            int value = array[i];
+            while (key >0 && array[key-1] > value){
+                array[key] = array[key-1];
+                key--;
+            }
+            array[key] = value;
+        }
+    }
+
+    public static void kuaisu1118(int[] array, int low, int high){
+        if(high <= low) return;
+        int vow = helper2(array, 0, array.length);
+        kuaisu1118(array, 0, vow-1);
+        kuaisu1118(array, vow + 1, high);
+    }
+
+    public static int helper2(int[] array, int low, int high){
+        int vow = array[low];
+        while (low < high){
+            while (low < high && array[high]>vow){
+                high--;
+            }
+            array[low]=array[high];
+            while (low < high && array[low]<vow){
+                low++;
+            }
+            array[high] = array[low];
+        }
+        array[low]=vow;
+        return vow;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
