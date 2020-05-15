6dd5172047f18bfb9366928c52278e7a431a864e
songpanfei <571354831@qq.com>
2020-05-15 19:46:37 +0800

    责任链和命令模式

---
+++ src/main/java/util/io/nio/NioServer.java
@@ -0,0 +1,86 @@
+package util.io.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by songpanfei on 2020-05-15.
+ */
+
+
+public class NioServer {
+
+    public static void main(String[] args) throws IOException {
+        // 1. serverSelector负责轮询是否有新的连接
+        Selector serverSelector = Selector.open();
+        // 2. clientSelector负责轮询连接是否有数据可读  (也可以server和client合二为一 即处理连接也处理读、写请求)
+        Selector clientSelector = Selector.open();
+        new Thread(() -> {
+            try {
+                // 对应IO编程中服务端启动
+                ServerSocketChannel listenerChannel = ServerSocketChannel.open();
+                listenerChannel.socket().bind(new InetSocketAddress(8800));
+                listenerChannel.configureBlocking(false);
+                listenerChannel.register(serverSelector, SelectionKey.OP_ACCEPT);
+                while (true) {
+                    if (serverSelector.select(1) > 0) {
+                        Set<SelectionKey> set = serverSelector.selectedKeys();
+                        Iterator<SelectionKey> keyIterator = set.iterator();
+                        while (keyIterator.hasNext()) {
+                            SelectionKey key = keyIterator.next();
+                            if (key.isAcceptable()) {
+                                try {
+                                    // 每来一个新连接，不需要创建一个线程，而是直接注册到clientSelector
+                                    SocketChannel clientChannel = ((ServerSocketChannel) key.channel()).accept();
+                                    clientChannel.configureBlocking(false);
+                                    clientChannel.register(clientSelector, SelectionKey.OP_READ);
+                                } finally {
+                                    keyIterator.remove();
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (IOException ignored) {
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                while (true) {
+                    if (clientSelector.select(1) > 0) {
+                        Set<SelectionKey> set = clientSelector.selectedKeys();
+                        Iterator<SelectionKey> keyIterator = set.iterator();
+                        while (keyIterator.hasNext()) {
+                            SelectionKey key = keyIterator.next();
+                            if (key.isReadable()) {
+                                try {
+                                    SocketChannel clientChannel = (SocketChannel) key.channel();
+                                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                                    // (3) 面向 Buffer
+                                    clientChannel.read(byteBuffer);
+                                    byteBuffer.flip();
+                                    System.out.println(Charset.defaultCharset().newDecoder().decode(byteBuffer).toString());
+                                } finally {
+                                    keyIterator.remove();
+                                    key.interestOps(SelectionKey.OP_READ);
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (IOException ignored) {
+            }
+        }).start();
+    }
+}
+
+
