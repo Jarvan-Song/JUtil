9eec97d33c66e53931c95fb4c4d2735f8059be89
songpanfei <songpanfei@sogou-inc.com>
2022-06-24 17:33:12 +0800

    代码提交测试

--- src/main/java/util/leetcode/SortAlgorithms.java
+++ src/main/java/util/leetcode/SortAlgorithms.java
@@ -269,6 +269,41 @@ public class SortAlgorithms {
         }
     }
 
+    //基数排序
+    public static int[] radixSort(int[] arr) {
+        if (arr.length < 2) {
+            return arr;
+        }
+        int N = 1;
+        int maxValue = arr[0];
+        for (int element : arr) {
+            if (element > maxValue) {
+                maxValue = element;
+            }
+        }
+        while (maxValue / 10 != 0) {
+            maxValue = maxValue / 10;
+            N += 1;
+        }
+        for (int i = 0; i < N; i++) {
+            List<List<Integer>> radix = new ArrayList<>();
+            for (int k = 0; k < 10; k++) {
+                radix.add(new ArrayList<Integer>());
+            }
+            for (int element : arr) {
+                int idx = (element / (int) Math.pow(10, i)) % 10;
+                radix.get(idx).add(element);
+            }
+            int idx = 0;
+            for (List<Integer> l : radix) {
+                for (int n : l) {
+                    arr[idx++] = n;
+                }
+            }
+        }
+        return arr;
+    }
+
     public static void xier(int[] arr){
         int temp;
         for (int delta = arr.length/2; delta>=1; delta/=2){                              //对每个增量进行一次排序
--- src/main/java/util/leetcode/Trie.java
+++ src/main/java/util/leetcode/Trie.java
@@ -1,6 +1,9 @@
 package util.leetcode;
 
 import java.util.Base64;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
 
 /**
  * Created by songpanfei on 2020-08-05.
@@ -16,8 +19,34 @@ public class Trie {
         System.out.println(trie.search("tes"));
         System.out.println(trie.startsWith("tes"));
 
-        System.out.print(new String(Base64.getEncoder().encode("admin".getBytes()), "utf8"));
+        //测试CompletableFuture
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(Trie::getMoreData);
+        Future<Integer> f = future.whenComplete((v, e) -> {
+            System.out.println(v);
+            System.out.println("0000000000"+ e);
+        });
+        System.out.println(f.get());
+//        System.in.read();
     }
+
+
+    private static Random rand = new Random();
+    private static long t = System.currentTimeMillis();
+    static int getMoreData() {
+        System.out.println("begin to start compute");
+//        try {
+//            Thread.sleep(10000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+        System.out.println("end to start compute. passed " + (System.currentTimeMillis() - t)/1000 + " seconds");
+        if((System.currentTimeMillis()/1000)%2 ==0 ){
+            throw new RuntimeException("test");
+        }
+        return rand.nextInt(1000);
+    }
+
+
 
     class TrieNode {
         public char val;
